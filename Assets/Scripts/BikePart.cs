0e041a957022911f624c8060583f9ff4869f2b05
elaine2700 <easerrano.e@gmail.com>
2022-04-08 21:24:37 -0700

    Highlight some individual components

--- Assets/Scripts/BikePart.cs
+++ Assets/Scripts/BikePart.cs
@@ -1,22 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using TMPro;
 
-public class BikePart : MonoBehaviour
+public class BikePart : XRBaseInteractable
 {
-    [SerializeField] string name;
+    [SerializeField] string partName;
     [TextArea]
     [SerializeField] string description;
+    [SerializeField] GameObject infoCanvas;
+    [SerializeField] TextMeshProUGUI nameField;
+    [SerializeField] TextMeshProUGUI descriptionField;
 
-    // Start is called before the first frame update
-    void Start()
+
+
+    private void Start()
     {
-        
+        //infoCanvas = GameObject.Find("Name-Description Canvas");
+        //nameField = infoCanvas.transform.Find("Part Name Text").GetComponent<TextMeshProUGUI>();
+        //descriptionField = infoCanvas.transform.Find("Description Text").GetComponent<TextMeshProUGUI>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DisplayInfo()
     {
-        
+        infoCanvas.SetActive(true);
+        nameField.text = partName;
+        descriptionField.text = description;
+        Debug.Log(partName);
+        Debug.Log(description);
     }
+
+    public void HideInfo()
+    {
+        infoCanvas.SetActive(false);
+    }
+
 }
