fcccce347f5d46d1a37b46e98830489fd006f7d7
Elaine <easerrano.e@gmail.com>
2022-04-14 15:07:40 -0700

    Make screenshots

---
+++ Assets/Scripts/Capture.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Capture : MonoBehaviour
+{
+    [SerializeField] InputActionReference captureInputReference;
+    int numCapture;
+    MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        captureInputReference.action.started += CaptureScreenShot;
+    }
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
+        numCapture = 1;
+    }
+
+    // Capture screenshots with Grip Button.
+    void CaptureScreenShot(InputAction.CallbackContext context)
+    {
+        string nameCapture = $"Capture{numCapture}";
+        ScreenCapture.CaptureScreenshot(nameCapture, ScreenCapture.StereoScreenCaptureMode.BothEyes);
+        meshRenderer.enabled = true;
+        numCapture++;
+        Invoke(nameof(HideText), 2f);
+    }
+
+    void HideText()
+    {
+        meshRenderer.enabled = false;
+    }
+}
