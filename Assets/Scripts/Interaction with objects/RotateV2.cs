26b8bfdf35ee4872da299a4a8d48f04544a79492
MymiRC <stephania@family.com.mx>
2022-04-14 15:36:56 -0700

    more tests

--- Assets/Scripts/Interaction with objects/RotateV2.cs
+++ Assets/Scripts/Interaction with objects/RotateV2.cs
@@ -28,23 +28,39 @@ public class RotateV2 : MonoBehaviour
     private void Update()
     {
 
-        xoffatehr = target2.localRotation.ToEulerAngles();
+        xoffatehr = target2.rotation.ToEulerAngles();
         Vector3 xoftarget3 = target3.localRotation.ToEulerAngles();
-        //Debug.Log($"local Rotation in x of parent is {xoffatehr}");
+        Debug.Log($"local Rotation x axis  of target2 is {xoffatehr}");
 
         //rotates with object as mirror
-        //target.rotation = Quaternion.Euler(initialRotation.x, xoftarget3.y * 90, initialRotation.z);
+        target.rotation = Quaternion.Euler(initialRotation.x, xoftarget3.y * 90, initialRotation.z);
 
-        //3rd try
-        //var rotation = Quaternion.LookRotation(target2.position);
-        //target.rotation = Quaternion.Slerp(target.rotation, rotation, Time.deltaTime * 1000);
 
         //4th try, with one hand works
         //Vector3 targetPosition = new Vector3(0, target2.position.x * 90, 0);
-        //target.rotation = Quaternion.Euler(targetPosition*-1);
+        //target.rotation = Quaternion.Euler(targetPosition * -1);
 
         //5tth try,  works but flips when points of origin are equal
         //target.rotation = Quaternion.Euler(initialRotation.x, xoffatehr.y* 90, initialRotation.z);
+
+        //3rd try no idea of how this works
+        //var rotation = Quaternion.LookRotation(target2.position);
+        //target.rotation = Quaternion.Slerp(target.rotation, rotation, Time.deltaTime *50);
+
+
+
+
+
+        target2.position = (left.position + right.position) / 2;
+        target2.rotation = Quaternion.LookRotation(left.position - right.position, Vector3.up);
+
+        //https://answers.unity.com/questions/408663/rotate-the-object-based-on-the-two-point.html
+        //float differenceposition = Vector3.AngleBetween(right.position, left.position);
+        //target2.rotation = Quaternion.Euler(0, differenceposition * 30 * 30, 0);
+
+
+
+
 
 
 
@@ -56,12 +72,5 @@ public class RotateV2 : MonoBehaviour
         //target.Rotate(0,(initialRotation.y) * movex,0, Space.Self);
         //target.RotateAround(target.localPosition, target.localPosition, result);
         //= initialRotation.ToEulerAngles * (Vector3.forward * result);
-
-        target2.position = (left.position + right.position) / 2;
-        target2.rotation = Quaternion.LookRotation(left.position - right.position, Vector3.up);
-
-        //https://answers.unity.com/questions/408663/rotate-the-object-based-on-the-two-point.html
-        //float differenceposition = Vector3.AngleBetween(right.position, left.position);
-        //target2.rotation = Quaternion.Euler(0, differenceposition * 30 * 30, 0);
     }
 }
